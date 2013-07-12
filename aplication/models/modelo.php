e638c59ad55efd28ace5d9ba2fcb412f3cb0c04c
MEXICANO8 <victor.mexicano@hotmail.com>
2013-07-12 08:54:05 -0500

    PRACTICA CLASE

--- aplication/models/modelo.php
+++ aplication/models/modelo.php
@@ -1,15 +1,20 @@
 <?php
 
-	class Modelo{
-	
-		private $db;
-		
-		function Modelo(){
-		
-			$this->db = ADONewConnection('mysql');
-		
-		}
-	
-	}
+    class Modelo{
+
+        private $db;
+
+        function Modelo(){
+                $this->db = ADONewConnection('mysql');
+                $this->db->debug = true;
+                $this->db->Connect('localhost','root','','PROYECTO');
+        }
+
+        public function consulta_datos(){
+            $rs = $this->db->Execute('select * from'. $this->nombre_tabla);
+            return $rs;
+        }
+
+    }
 	
 ?>
--- aplication/models/usuarios.php
+++ aplication/models/usuarios.php
@@ -1,89 +1,94 @@
 <?php
 
-class Usuarios{
+    class Usuarios extends Modelo{
 
-	private id_asistente;
-	private apellido_paterno;
-	private apellido_materno;
-	private nombre;
-	private sexo;
-	private edad;
-	private email;
-	private nctr_rfc;
-	private numero_control;
-	
-	public get_id_asistente(){
-		return $this->id_asistente;
-	}
+        public  $nombre_tabla = 'usuarios';
+        private $id_asistente;
+        private $apellido_paterno;
+        private $apellido_materno;
+        private $nombre;
+        private $sexo;
+        private $edad;
+        private $email;
+        private $nctr_rfc;
+        private $numero_control;
 
-	public set_id_asistente($valor){
-		$this->id_asistente=$valor;
-	}
-	
-	public get_apellido_paterno(){
-		return $this->apellido_paterno;
-	}
+        function __construct() {
+            $this->Modelo();
+        } 
 
-	public set_apellido_paterno($valor){
-		$this->apellido_paterno=$valor;
-	}
-	
-	public get_apellido_materno(){
-		return $this->apellido_materno;
-	}
+        public function get_id_asistente(){
+                return $this->id_asistente;
+        }
 
-	public set_apellido_materno($valor){
-		$this->apellido_materno=$valor;
-	}
-	
-	public get_nombre(){
-		return $this->nombre;
-	}
+        public function set_id_asistente($valor){
+                $this->id_asistente=$valor;
+        }
 
-	public set_nombre($valor){
-		$this->nombre=$valor;
-	}
-	
-	public get_sexo(){
-		return $this->sexo;
-	}
+        public function get_apellido_paterno(){
+                return $this->apellido_paterno;
+        }
 
-	public set_sexo($valor){
-		$this->sexo=$valor;
-	}
-	
-	public get_edad(){
-		return $this->edad;
-	}
+        public function set_apellido_paterno($valor){
+                $this->apellido_paterno=$valor;
+        }
 
-	public set_edad($valor){
-		$this->edad=$valor;
-	}
-	
-	public get_email(){
-		return $this->email;
-	}
+        public function get_apellido_materno(){
+                return $this->apellido_materno;
+        }
 
-	public set_email($valor){
-		$this->email=$valor;
-	}
-	
-	public get_nctr_rfc(){
-		return $this->nctr_rfc;
-	}
+        public function set_apellido_materno($valor){
+                $this->apellido_materno=$valor;
+        }
 
-	public set_nctr_rfc($valor){
-		$this->nctr_rfc=$valor;
-	}
-	
-	public get_numero_control(){
-		return $this->numero_control;
-	}
+        public function get_nombre(){
+                return $this->nombre;
+        }
 
-	public set_numero_control($valor){
-		$this->numero_control=$valor;
-	}
+        public function set_nombre($valor){
+                $this->nombre=$valor;
+        }
 
-}
+        public function get_sexo(){
+                return $this->sexo;
+        }
+
+        public function set_sexo($valor){
+                $this->sexo=$valor;
+        }
+
+        public function get_edad(){
+                return $this->edad;
+        }
+
+        public function set_edad($valor){
+                $this->edad=$valor;
+        }
+
+        public function get_email(){
+                return $this->email;
+        }
+
+        public function set_email($valor){
+                $this->email=$valor;
+        }
+
+        public function get_nctr_rfc(){
+                return $this->nctr_rfc;
+        }
+
+        public function set_nctr_rfc($valor){
+                $this->nctr_rfc=$valor;
+        }
+
+        public function get_numero_control(){
+                return $this->numero_control;
+        }
+
+        public function set_numero_control($valor){
+                $this->numero_control=$valor;
+        }
+
+    }
 
 ?>
