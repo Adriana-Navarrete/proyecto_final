5a2d583c56e46c22b3e76557bd85932900732cf9
MEXICANO8 <victor.mexicano@hotmail.com>
2013-07-13 08:18:02 -0500

    INSERTA DATOS

--- aplication/controllers/sitecontroller/registrocontroller.php
+++ aplication/controllers/sitecontroller/registrocontroller.php
@@ -1,11 +1,12 @@
 <pre>
 <?php
     
+    include ('../../models/modelo.php');    
     include ('../../models/usuarios.php');
-    include ('../../models/modelo.php');
     include ('../../libs/adodb5/adodb.inc.php');
     
     $usuario = new Usuarios();
+    $usuario->inserta_datos();
     $rs = $usuario->consulta_datos();
     print_r($rs->GetRows());
 
--- aplication/models/modelo.php
+++ aplication/models/modelo.php
@@ -12,7 +12,28 @@
 
         public function consulta_datos(){
             $rs = $this->db->Execute('select * from '. $this->nombre_tabla);
+            $this->get_error($rs, 'Error en consulta datos');
             return $rs;
+        }
+        
+        public function inserta_datos(){
+            $rs = array();
+            $rs['apellido_paterno']='GALVAN';
+            $rs['apellido_materno']='MEXICANO';
+            $rs['nombre']='VICTOR';
+            $rs['sexo']='M';
+            $rs['edad']='21';
+            $rs['email']='victor.mexicano@hotmail.com';
+            $rs['nctr_rfc']='GAMV911104HY4';
+            $rs['numero_control']='09030791';
+            $sql_insert = $this->db->GetInsertSQL($this->nombre_tabla,$rs);
+            $this->get_error($this->db->Execute($sql_insert),'Error en Modelo.inserta');
+        }
+        
+        public function get_error($result, $tipo_error){
+            if($result == false){
+                die('Redireccionar a la pagina de error'.$tipo_error);
+            }
         }
 
     }
